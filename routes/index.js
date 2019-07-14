a16ec064d658f6ec83ff9492f46ea79bb5d3fe03
Nicolle Gonzalez <nicolle.ngonzalez@gmail.com>
2019-07-13 22:01:36 -0400

    Passport

---
+++ routes/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router  = express.Router();
+
+
+
+
+const ensureLogin = require("connect-ensure-login");
+
+/* GET home page */
+router.get('/', (req, res, next) => {
+
+  console.log('=-=--=-=--=',req.session)
+  console.log('=-=--=-=--=',req.user)
+
+  res.render('index');
+});
+
+
+router.get('/blah', ensureLogin.ensureLoggedIn('/login') ,(req, res, next)=>{
+// router.get('/blah',(req, res, next)=>{
+
+  // this is how you can manually add something to req.flash
+  req.flash('error', '*****************Random Word******************')
+  // res.redirect('/')
+  res.render('blah');
+})
+
+module.exports = router;
